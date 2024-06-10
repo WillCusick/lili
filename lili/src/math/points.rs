16018c6466c6cfddca45a8a51f361807df0c986c
Will Cusick <william.m.cusick@gmail.com>
2024-06-09 21:28:50 -0400

    3.4 Finish points

--- lili/src/math/points.rs
+++ lili/src/math/points.rs
@@ -1,17 +1,84 @@
 //! Point types and operations.
+use std::ops::Sub;
+
 use auto_ops::{impl_op_ex, impl_op_ex_commutative};
 
 use crate::math::tuples::Tuple;
 
 use super::{
+    length::Length,
     tuples::{
         from_tuple_conv_impl2, from_tuple_conv_impl3, index_impl2, index_impl3, index_mut_impl2,
         index_mut_impl3, tuple2_impl, tuple3_impl, tuple_impl2, tuple_impl3, TupleElement,
     },
+    vectors::{Vector2f, Vector2i, Vector3f, Vector3i},
     Float,
 };
 
+trait Distance: Sub + Sized
+where
+    <Self as Sub>::Output: Length,
+{
+    fn distance(&self, other: &Self) -> <<Self as Sub>::Output as Length>::LengthType;
+    fn distance_squared(&self, other: &Self)
+        -> <<Self as Sub>::Output as Length>::TupleElementType;
+}
+
+impl Distance for Point2f {
+    fn distance(&self, other: &Self) -> <<Self as Sub>::Output as Length>::LengthType {
+        (self - other).length()
+    }
+
+    fn distance_squared(
+        &self,
+        other: &Self,
+    ) -> <<Self as Sub>::Output as Length>::TupleElementType {
+        (self - other).length_squared()
+    }
+}
+impl Distance for Point3f {
+    fn distance(&self, other: &Self) -> <<Self as Sub>::Output as Length>::LengthType {
+        (self - other).length()
+    }
+
+    fn distance_squared(
+        &self,
+        other: &Self,
+    ) -> <<Self as Sub>::Output as Length>::TupleElementType {
+        (self - other).length_squared()
+    }
+}
+
+impl Distance for Point2i {
+    fn distance(&self, other: &Self) -> <<Self as Sub>::Output as Length>::LengthType {
+        (self - other).length()
+    }
+
+    fn distance_squared(
+        &self,
+        other: &Self,
+    ) -> <<Self as Sub>::Output as Length>::TupleElementType {
+        (self - other).length_squared()
+    }
+}
+
+impl Distance for Point3i {
+    fn distance(&self, other: &Self) -> <<Self as Sub>::Output as Length>::LengthType {
+        (self - other).length()
+    }
+
+    fn distance_squared(
+        &self,
+        other: &Self,
+    ) -> <<Self as Sub>::Output as Length>::TupleElementType {
+        (self - other).length_squared()
+    }
+}
+
 /// Represents a 2-dimensional point.
+///
+/// A point is a zero-dimensional location in space.
+/// It is represented by a pair of coordinates.
 #[derive(Default, Clone)]
 pub struct Point2<T> {
     /// The x-coordinate of the point.
@@ -83,14 +150,14 @@ impl_op_ex!(+ |a: &Point3f, b: &Point3f| -> Point3f { Point3f::new(a.x + b.x, a.
 impl_op_ex!(+ |a: &Point2i, b: &Point2i| -> Point2i { Point2i::new(a.x + b.x, a.y + b.y) });
 impl_op_ex!(+ |a: &Point3i, b: &Point3i| -> Point3i { Point3i::new(a.x + b.x, a.y + b.y, a.z + b.z) });
 
-// Point - Point
-impl_op_ex!(-|a: &Point2f, b: &Point2f| -> Point2f { Point2f::new(a.x - b.x, a.y - b.y) });
-impl_op_ex!(-|a: &Point3f, b: &Point3f| -> Point3f {
-    Point3f::new(a.x - b.x, a.y - b.y, a.z - b.z)
+// Point - Point (= Vector)
+impl_op_ex!(-|a: &Point2f, b: &Point2f| -> Vector2f { Vector2f::new(a.x - b.x, a.y - b.y) });
+impl_op_ex!(-|a: &Point3f, b: &Point3f| -> Vector3f {
+    Vector3f::new(a.x - b.x, a.y - b.y, a.z - b.z)
 });
-impl_op_ex!(-|a: &Point2i, b: &Point2i| -> Point2i { Point2i::new(a.x - b.x, a.y - b.y) });
-impl_op_ex!(-|a: &Point3i, b: &Point3i| -> Point3i {
-    Point3i::new(a.x - b.x, a.y - b.y, a.z - b.z)
+impl_op_ex!(-|a: &Point2i, b: &Point2i| -> Vector2i { Vector2i::new(a.x - b.x, a.y - b.y) });
+impl_op_ex!(-|a: &Point3i, b: &Point3i| -> Vector3i {
+    Vector3i::new(a.x - b.x, a.y - b.y, a.z - b.z)
 });
 
 // Point * Scalar
@@ -154,3 +221,15 @@ from_tuple_conv_impl2!(Point2i, Point2f, Float);
 from_tuple_conv_impl3!(Point3i, Point3f, Float);
 from_tuple_conv_impl2!(&Point2i, Point2f, Float);
 from_tuple_conv_impl3!(&Point3i, Point3f, Float);
+
+// Point + Vector
+impl_op_ex!(+ |a: &Point2f, b: &Vector2f| -> Point2f { Point2f::new(a.x + b.x, a.y + b.y)});
+impl_op_ex!(+ |a: &Point3f, b: &Vector3f| -> Point3f { Point3f::new(a.x + b.x, a.y + b.y, a.z + b.z)});
+impl_op_ex!(+ |a: &Point2i, b: &Vector2i| -> Point2i { Point2i::new(a.x + b.x, a.y + b.y)});
+impl_op_ex!(+ |a: &Point3i, b: &Vector3i| -> Point3i { Point3i::new(a.x + b.x, a.y + b.y, a.z + b.z)});
+
+// Point += Vector
+impl_op_ex!(+= |a: &mut Point2f, b: &Vector2f| { a.x += b.x; a.y += b.y; });
+impl_op_ex!(+= |a: &mut Point3f, b: &Vector3f| { a.x += b.x; a.y += b.y; a.z += b.z; });
+impl_op_ex!(+= |a: &mut Point2i, b: &Vector2i| { a.x += b.x; a.y += b.y; });
+impl_op_ex!(+= |a: &mut Point3i, b: &Vector3i| { a.x += b.x; a.y += b.y; a.z += b.z; });
